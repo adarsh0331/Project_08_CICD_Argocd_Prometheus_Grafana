12b74a926d62bf92eccb3a0510e0d2cb0af3df3e
Barkunta Adarsh <129056162+adarsh0331@users.noreply.github.com>
2025-09-22 19:17:44 +0530

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,169 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Key Pair (use your existing public key)
+resource "aws_key_pair" "mykey" {
+  key_name   = "mykey"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+# Security Group
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SonarQube"
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ----------------------------
+# Instance 1: jenkins
+# ----------------------------
+resource "aws_instance" "Jenkins_server" {
+  ami           = "ami-08982f1c5bf93d976" # Amazon Linux 2 (us-east-1)
+  instance_type = "t2.large"
+  key_name      = aws_key_pair.mykey.key_name
+  security_groups = [aws_security_group.ec2_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+
+              sudo yum install git -y
+  
+              # Install Docker
+
+              echo ">>> Installing Docker..."
+              sudo yum install -y docker
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo usermod -aG docker $USER
+
+              # Install Jenkins
+
+              echo ">>> Installing Jenkins..."
+
+              sudo yum update -y
+              sudo wget -O /etc/yum.repos.d/jenkins.repo  https://pkg.jenkins.io/redhat-stable/jenkins.repo
+              sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+              sudo yum upgrade
+              sudo yum install java-21-amazon-corretto -y
+              sudo yum install jenkins -y
+              sudo systemctl enable jenkins
+              sudo systemctl start jenkins
+
+
+              # Install Trivy
+
+              echo ">>> Installing Trivy..."
+              sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/latest/download/trivy_0.56.2_Linux-64bit.rpm
+
+
+              # Install SonarQube (with Docker)
+ 
+              echo ">>> Installing SonarQube using Docker..."
+              # Pull and run SonarQube container
+              sudo docker run -d --name sonarqube \
+              -p 9000:9000 \
+              sonarqube:lts
+
+              # Install Node.js + npm
+
+              echo ">>> Installing Node.js & npm..."
+              curl -sL https://rpm.nodesource.com/setup_18.x | sudo bash -
+              sudo yum install -y nodejs
+
+
+              # Final Info
+
+              echo ">>> Installation Completed!"
+              echo "Jenkins running on: http://<your-server-ip>:8080"
+              echo "SonarQube running on: http://<your-server-ip>:9000"
+              echo "Use 'sudo cat /var/lib/jenkins/secrets/initialAdminPassword' for Jenkins password."
+              EOF
+
+  tags = {
+    Name = "jenkins-Server"
+  }
+}
+
+# ----------------------------
+# Instance 2: k8s Server
+# ----------------------------
+resource "aws_instance" "k8s_server" {
+  ami           = "ami-08982f1c5bf93d976"
+  instance_type = "t2.xlarge"
+  key_name      = aws_key_pair.mykey.key_name
+  security_groups = [aws_security_group.ec2_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              
+              # Install dependencies
+
+              echo ">>> Installing required packages..."
+              sudo yum install -y curl wget git tar
+
+              # Install kubectl
+
+              echo ">>> Installing kubectl..."
+              curl -LO "https://dl.k8s.io/release/$(curl -sSL https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+              chmod +x kubectl
+              sudo mv kubectl /usr/local/bin/
+              kubectl version --client
+              curl -sSL "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz" -o eksctl.tar.gz
+              tar -xzf eksctl.tar.gz
+              sudo mv eksctl /usr/local/bin/
+              eksctl version
+
+
+              # Install Helm
+
+              echo ">>> Installing Helm..."
+              curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+
+              # Add Helm Repos
+
+              echo ">>> Adding Helm repositories..."
+              helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+              helm repo add grafana https://grafana.github.io/helm-charts
+              helm repo add argo https://argoproj.github.io/argo-helm
+              helm repo update
+
+              EOF
+
+  tags = {
+    Name = "k8s-Server"
+  }
+}
